bd715981fa4b4b56d2c87ea33c7b86b4b5dcec14
Michael Ciabuca <ciabuca@gmail.com>
2020-07-20 00:12:32 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,11 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each_with_index do | value, index |
+    puts "#{index + 1}. #{value}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  
 end
 
 def long_planeteer_calls# code an argument here
