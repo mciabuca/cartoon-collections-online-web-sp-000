3a95806a960a889a4c960b80342b5e72b5cc4693
Michael Ciabuca <ciabuca@gmail.com>
2020-07-20 00:32:43 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,7 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  res1 = array.all? { |num| array[num].length > 4}  
+  res1 = array.any? { |num| array[num].length > 4}  
   return res1 
 end
 
